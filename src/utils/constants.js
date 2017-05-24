f7d3ec394fe51280033636c5c548bdd43836bddd
zhangwg <zhangwg@broada.com>
2017-05-24 20:58:15 +0800

    fix: 修改request

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export default {
-    //"api_root": "api/v2"
+    "api_root": "api/v2"
     // "api_root": "http://10.1.50.58:8180/api/v2"
     // "api_root": "http://10.1.10.124:8100/api/v2"
     // "api_root": "http://10.1.250.129:8180/api/v2"
@@ -7,7 +7,7 @@ export default {
     // "api_root": "http://10.1.251.102:8100/api/v2"
     // "api_root": "http://alert.uyundev.cn/api/v2"
     //"api_root": "http://10.1.10.27:8100/api/v2" // gu
-    "api_root": "http://10.1.250.32:8100/api/v2" // zhao
+    //"api_root": "http://10.1.250.32:8100/api/v2" // zhao
     // "api_root": "http://10.1.251.102:8100/api/v2" // fang
     // "api_root": "http://10.1.200.105:8100/api/v2"
     // "api_root": "http://10.1.251.102:8100/api/v2" // 方鑫_开发环境
