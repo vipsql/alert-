f69dd43dc97ca2f70842d2f11f8888d742da96bc
zhangwg <zhangwg@broada.com>
2017-06-12 13:45:54 +0800

    fix: 修改BUG

--- src/models/alertQueryDetail.js
+++ src/models/alertQueryDetail.js
@@ -524,7 +524,7 @@ export default {
                   col.checked = !col.checked;
               }
               if (col.checked) {
-                  if (col.id == 'source' || col.id == 'lastTime' || col.id == 'lastOccurTime' || col.id == 'count') {
+                  if (col.id == 'source' || col.id == 'lastTime' || col.id == 'lastOccurTime' || col.id == 'count' || col.id == 'status') {
                       arr.push({ key: col.id, title: col.name, order: true }) // order字段先定死
                   } else {
                       arr.push({ key: col.id, title: col.name }) // width先定死
