82260bdb7ce15d917b5babaa790e3fb276d5eb0a
yuansl <yuansl@uyunsoft.cn>
2017-07-04 09:26:31 +0800

    chore: format the visula analyze code

--- src/components/alertList/visualAnalyze/index.js
+++ src/components/alertList/visualAnalyze/index.js
@@ -5,52 +5,52 @@ import VisualAnalyze from './visualAnalyze'
 import styles from '../index.less'
 import $ from 'jquery'
 
-const  VisualAnalyzeWrap = ({dispatch, visualAnalyze}) => {
-  
+const VisualAnalyzeWrap = ({ dispatch, visualAnalyze }) => {
+
   const props = {
     ...visualAnalyze,
-    
-    showResList(e){
-        const target = e.target
-        if(target.className.indexOf('tagsGroup') > -1) return
-        const parentNode = target.parentNode,
-              gr2Val = parentNode.getAttribute('data-gr2Val'),
-              gr3Val = parentNode.getAttribute('data-gr3Val')
 
-              localStorage.setItem('__alert_visualAnalyze_gr2Val',gr2Val)
-              localStorage.setItem('__alert_visualAnalyze_gr3Val',gr3Val)
-        const $_parentNode = $(parentNode),
-              offset = $_parentNode.offset(),
-              left = offset.left,
-              width = $_parentNode.width()/2,
-              top = offset.top
-        
-        const cloneNode = $('<div/>')
-        cloneNode.addClass('tagsGroupAnimation').css({
-            left: left + width,
-            top: top
-        }).appendTo('body')
-        const targetEle = $('#visualGr2'),
-              targetLeft =  targetEle.offset().left,
-              targetTop =  targetEle.offset().top
-              
-        cloneNode.animate({
-            left: targetLeft + 90,
-            top: targetTop
-        },300,() => {
-            cloneNode.remove()
-             dispatch({
-                type: 'visualAnalyze/queryVisualRes'
-            })
-            dispatch({
-                type: 'visualAnalyze/queryResInfo',
-                payload: {
-                    res: parentNode.getAttribute('data-gr3Val')
-                }
-            })
+    showResList(e) {
+      const target = e.target
+      if (target.className.indexOf('tagsGroup') > -1) return
+      const parentNode = target.parentNode,
+        gr2Val = parentNode.getAttribute('data-gr2Val'),
+        gr3Val = parentNode.getAttribute('data-gr3Val')
+
+      localStorage.setItem('__alert_visualAnalyze_gr2Val', gr2Val)
+      localStorage.setItem('__alert_visualAnalyze_gr3Val', gr3Val)
+      const $_parentNode = $(parentNode),
+        offset = $_parentNode.offset(),
+        left = offset.left,
+        width = $_parentNode.width() / 2,
+        top = offset.top
+
+      const cloneNode = $('<div/>')
+      cloneNode.addClass('tagsGroupAnimation').css({
+        left: left + width,
+        top: top
+      }).appendTo('body')
+      const targetEle = $('#visualGr2'),
+        targetLeft = targetEle.offset().left,
+        targetTop = targetEle.offset().top
+
+      cloneNode.animate({
+        left: targetLeft + 90,
+        top: targetTop
+      }, 300, () => {
+        cloneNode.remove()
+        dispatch({
+          type: 'visualAnalyze/queryVisualRes'
         })
-       
-        
+        dispatch({
+          type: 'visualAnalyze/queryResInfo',
+          payload: {
+            res: parentNode.getAttribute('data-gr3Val')
+          }
+        })
+      })
+
+
     },
     detailClick(e) {
       const alertId = e.target.getAttribute('data-id')
@@ -59,89 +59,89 @@ const  VisualAnalyzeWrap = ({dispatch, visualAnalyze}) => {
         payload: alertId
       })
     },
-    showAlertList(e){
-        // 首先初始数据
+    showAlertList(e) {
+      // 首先初始数据
+      dispatch({
+        type: 'visualAnalyze/updateAlertList',
+        payload: ''
+      })
+      let target = e.target
+      while (target.tagName.toLowerCase() != 'li') {
+        target = target.parentNode
+      }
+      window.__uyun_showAlertList = setTimeout(() => {
         dispatch({
-            type: 'visualAnalyze/updateAlertList',
-            payload: ''
+          type: 'visualAnalyze/showAlertList',
+          payload: target.getAttribute('data-id')
         })
-        let target = e.target
-        while(target.tagName.toLowerCase() != 'li'){
-            target = target.parentNode
-        }
-        window.__uyun_showAlertList = setTimeout(() => {
-            dispatch({
-                type: 'visualAnalyze/showAlertList',
-                payload: target.getAttribute('data-id')
-            })
-        },300)
-        
+      }, 300)
+
     },
-    cancelShowAlertList(){
-        clearTimeout(window.__uyun_showAlertList)
-        
+    cancelShowAlertList() {
+      clearTimeout(window.__uyun_showAlertList)
+
     },
     handleExpand(e) {
-        const target = e.target,
+      const target = e.target,
         isExpand = target.getAttribute('data-expand') == 'true' ? false : true,
-        index = target.getAttribute('data-index') 
-        
-        dispatch({
-            type: 'visualAnalyze/expandList',
-            payload: {
-                index,
-                isExpand
-            }
-        })
+        index = target.getAttribute('data-index')
+
+      dispatch({
+        type: 'visualAnalyze/expandList',
+        payload: {
+          index,
+          isExpand
+        }
+      })
     },
-    redirectTagsList(){
-        // localStorage.removeItem('__alert_visualAnalyze_gr4')
-        dispatch({
-            type: 'visualAnalyze/redirectTagsList'
-        })
+    redirectTagsList() {
+      // localStorage.removeItem('__alert_visualAnalyze_gr4')
+      dispatch({
+        type: 'visualAnalyze/redirectTagsList'
+      })
     },
-    gr2Change(value){
-        localStorage.setItem('__alert_visualAnalyze_gr2', value)
-        dispatch({
-            type: 'visualAnalyze/queryVisualList',
-            payload: {
-                isFirst: false
-            }
-        })
+    gr2Change(value) {
+      localStorage.setItem('__alert_visualAnalyze_gr2', value)
+      dispatch({
+        type: 'visualAnalyze/queryVisualList',
+        payload: {
+          isFirst: false
+        }
+      })
     },
-    gr3Change(value){
-        localStorage.setItem('__alert_visualAnalyze_gr3', value)
-        dispatch({
-            type: 'visualAnalyze/queryVisualList',
-            payload: {
-                isFirst: false
-            }
-        })
+    gr3Change(value) {
+      localStorage.setItem('__alert_visualAnalyze_gr3', value)
+      dispatch({
+        type: 'visualAnalyze/queryVisualList',
+        payload: {
+          isFirst: false
+        }
+      })
     },
-    gr4Change(value){
-        localStorage.setItem('__alert_visualAnalyze_gr4', value)
-        dispatch({
-            type: 'visualAnalyze/queryVisualRes'
-        })
+    gr4Change(value) {
+      localStorage.setItem('__alert_visualAnalyze_gr4', value)
+      dispatch({
+        type: 'visualAnalyze/queryVisualRes'
+      })
     },
 
-    showIncidentGroup(e){
-        const checked = e.target.checked
-        dispatch({
-            type: 'visualAnalyze/queryVisualList',
-            payload: {
-                isFirst: false,
-                showIncidentGroup: checked
-            }
-        })
-        dispatch({
-            type: 'visualAnalyze/updateIncidentGroup',
-            payload: checked
-        })
-        
+    showIncidentGroup(e) {
+      const checked = e.target.checked
+      dispatch({
+        type: 'visualAnalyze/queryVisualList',
+        payload: {
+          isFirst: false,
+          showIncidentGroup: checked
+        }
+      })
+      dispatch({
+        type: 'visualAnalyze/updateIncidentGroup',
+        payload: checked
+      })
+
 
     }
-    
+
 
   }
 
