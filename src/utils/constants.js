174d1bbcae1de36483db9b6a5bad2ab150be4152
zhangsf <zhangsf@broada.com>
2017-08-29 17:13:36 +0800

    fix:修复“webHook中API文档链接404”的bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 export default {
-    "api_root": "api/v2",
-    "webhook_api": "/help/chapter5/5-1.html", // webhook接出应用提示中用到的链接
-    // "api_root": "http://10.1.50.58:8100/api/v2"
+    // "api_root": "api/v2",
+    "webhook_api": "./help/chapter5/5-1.html", // webhook接出应用提示中用到的链接
+    // "api_root": "api/v2"
+    "api_root": "http://10.1.50.58:8100/api/v2"
     // "api_root": "http://10.1.10.124:8100/api/v2"
     // "api_root": "http://10.1.250.129:8180/api/v2"
     //"api_root": "http://10.1.250.32:8100/api/v2"
